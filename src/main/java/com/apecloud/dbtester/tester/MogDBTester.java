315d49344b6428888eaa07373f940b282f3aeada
huangzhangshu <huangzhangshu@apecloud.com>
2025-01-15 19:29:02 +0800

    feat: add mogdb executionLoop test

--- src/main/java/com/apecloud/dbtester/tester/MogDBTester.java
+++ src/main/java/com/apecloud/dbtester/tester/MogDBTester.java
@@ -13,6 +13,7 @@ public class MogDBTester implements DatabaseTester {
     private List<DatabaseConnection> connections = new ArrayList<>();
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
     private final DBConfig dbConfig;
+    private String databaseConnection = "postgres";
 
     // 默认构造函数
     public MogDBTester() {
@@ -33,27 +34,44 @@ public class MogDBTester implements DatabaseTester {
         try {
             Class.forName("io.mogdb.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("PostgreSQL JDBC Driver not found, please try again..", e);
+            throw new RuntimeException("Mogdb JDBC Driver not found, please try again..", e);
         }
 
         String url = String.format("jdbc:mogdb://%s:%d/%s?useSSL=false&serverTimezone=UTC",
                 dbConfig.getHost(),
                 dbConfig.getPort(),
                 dbConfig.getDatabase());
+
+        String url2 = String.format("jdbc:mogdb://%s:%d/%s?useSSL=false&serverTimezone=UTC",
+                dbConfig.getHost(),
+                dbConfig.getPort(),
+                databaseConnection);
+
+
+        if (dbConfig.getDatabase() == null || dbConfig.getDatabase().equals("")) {
+            url = url2;
+        }
+
         try {
-            return new PostgreSQLConnection(DriverManager.getConnection(url, dbConfig.getUser(), dbConfig.getPassword()));
+            return new MogdbConnection(DriverManager.getConnection(url, dbConfig.getUser(), dbConfig.getPassword()));
         } catch (SQLException e) {
-            throw new IOException("Failed to connect to PostgreSQL database", e);
+            System.err.println("Failed to connect to Mogdb database: " + e );
+            System.err.println("Trying with database mogdb.");
+            try {
+                return new MogdbConnection(DriverManager.getConnection(url2, dbConfig.getUser(), dbConfig.getPassword()));
+            } catch (SQLException e2) {
+                throw new IOException("Failed to connect to Mogdb database: ", e2);
+            }
         }
     }
 
     @Override
     public QueryResult execute(DatabaseConnection connection, String query) throws IOException {
-        PostgreSQLConnection postgresConnection = (PostgreSQLConnection) connection;
+        MogdbConnection postgresConnection = (MogdbConnection) connection;
         try {
             Statement statement = postgresConnection.connection.createStatement();
             boolean isResultSet = statement.execute(query);
-            return new PostgreSQLQueryResult(statement.getResultSet(), statement.getUpdateCount());
+            return new MogdbQueryResult(statement.getResultSet(), statement.getUpdateCount());
         } catch (SQLException e) {
             throw new IOException("Failed to execute query", e);
         }
@@ -119,13 +137,163 @@ public class MogDBTester implements DatabaseTester {
 
     @Override
     public String executionLoop(DatabaseConnection connection, String query, int duration, int interval, String database, String table) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        StringBuilder result_db = new StringBuilder();
+        int successfulExecutions = 0;
+        int failedExecutions = 0;
+        int disconnectCounts = 0;
+        boolean executionError = false;
+
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + duration * 1000;
+        long errorTime = 0;
+        long recoveryTime;
+        long errorToRecoveryTime;
+        Date errorDate = null;
+        long lastOutputTime = System.currentTimeMillis();
+        int outputPassTime = 0;
+
+        int insert_index = 0;
+        int gen_test_query = 0;
+        String query_test;
+        String gen_test_values;
+
+        // check gen test query
+        if (query == null || query.equals("") || (database != null && !database.equals("")) || (table != null && !table.equals(""))) {
+            gen_test_query = 1;
+        }
+
+        if (database == null || database.equals("")) {
+            database = "executions_loop";
+        }
+
+        if (table == null || table.equals("")) {
+            table = "executions_loop_table";
+        }
+
+        System.out.println("Execution loop start:" + query);
+        while (System.currentTimeMillis() < endTime) {
+            insert_index = insert_index + 1;
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastOutputTime >= interval * 1000) {
+                outputPassTime = outputPassTime + interval;
+                lastOutputTime = currentTime;
+                System.out.println("[ " + outputPassTime + "s ] executions total: " + (successfulExecutions + failedExecutions)
+                        + " successful: " + successfulExecutions + " failed: " + failedExecutions
+                        + " disconnect: " + disconnectCounts);
+            }
+
+            try {
+                if (executionError) {
+                    Thread.sleep(1000);
+                    connection = this.connect();
+                }
+
+                if (gen_test_query == 1) {
+                    // check if database exists
+                    query_test = "SELECT datname FROM pg_database where datname = '" + database + "';";
+                    QueryResult queryResult = execute(connection, query_test);
+                    if (queryResult.hasResultSet()) {
+                        ResultSet rs = queryResult.getResultSet();
+                        if (rs.getMetaData() != null) {
+                            ResultSetMetaData metaData = rs.getMetaData();
+                            int columnCount = metaData.getColumnCount();
+                            while (rs.next()) {
+                                for (int i = 1; i <= columnCount; i++) {
+                                    result_db.append(rs.getString(i));
+                                }
+                            }
+                        }
+                    }
+
+                    if (result_db.toString().equals("")) {
+                        // create test databases
+                        System.out.println("create databases " + database);
+                        query_test = "CREATE DATABASE " + database + ";";
+                        execute(connection, query_test);
+                    }
+
+                    if (!databaseConnection.equals(database)) {
+                        System.out.println("reconnect connection " + database);
+                        databaseConnection = database;
+                        connection = this.connect();
+                    }
+
+                    if (table.equals("executions_loop_table")) {
+                        // drop test table
+                        System.out.println("drop table " + table);
+                        query_test = "DROP TABLE IF EXISTS " + table + ";";
+                        execute(connection, query_test);
+                    }
+
+                    // create test table
+                    System.out.println("create table " + table);
+                    query_test = "CREATE TABLE IF NOT EXISTS " + table + " (id SERIAL PRIMARY KEY , value text); ";
+                    execute(connection, query_test);
+
+                    gen_test_query = 2;
+                }
+
+                if ((gen_test_query == 2 && (query == null || query.equals("")) || gen_test_query == 3)) {
+                    gen_test_values = "executions_loop_test_" + insert_index;
+                    // set test query
+                    query = "INSERT INTO " + table + " (value) VALUES ('" + gen_test_values + "');";
+                    if (gen_test_query == 2) {
+                        System.out.println("Execution loop start:" + query);
+                    }
+                    gen_test_query = 3;
+                }
+
+                execute(connection, query);
+                successfulExecutions++;
+                if (executionError) {
+                    recoveryTime = System.currentTimeMillis();
+                    Date recoveryDate = new Date(recoveryTime);
+                    System.out.println("[" + sdf.format(errorDate) + "] Connection error occurred!");
+                    System.out.println("[" + sdf.format(recoveryDate) + "] Connection successfully recovered!");
+                    errorToRecoveryTime = recoveryTime - errorTime;
+                    System.out.println("The connection was restored in " + errorToRecoveryTime + " milliseconds.");
+                    executionError = false;
+                }
+            } catch (IOException e) {
+                failedExecutions++;
+                if (!executionError) {
+                    disconnectCounts++;
+                    errorTime = System.currentTimeMillis();
+                    errorDate = new Date(errorTime);
+                    System.out.println("[" + sdf.format(errorDate) + "] Connection error occurred!");
+                    executionError = true;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("[ " + duration + "s ] executions total: " + (successfulExecutions + failedExecutions)
+                + " successful: " + successfulExecutions + " failed: " + failedExecutions
+                + " disconnect: " + disconnectCounts);
+
+        releaseConnections();
+
+        result.append("Execution loop completed during ").append(duration).append(" seconds");
+
+        return String.format("Total Executions: %d\n" +
+                        "Successful Executions: %d\n" +
+                        "Failed Executions: %d\n" +
+                        "Disconnection Counts: %d",
+                successfulExecutions + failedExecutions,
+                successfulExecutions,
+                failedExecutions,
+                disconnectCounts);
     }
 
-    private static class PostgreSQLConnection implements DatabaseConnection {
+
+    private static class MogdbConnection implements DatabaseConnection {
         private final Connection connection;
 
-        PostgreSQLConnection(Connection connection) {
+        MogdbConnection(Connection connection) {
             this.connection = connection;
         }
 
@@ -134,16 +302,16 @@ public class MogDBTester implements DatabaseTester {
             try {
                 connection.close();
             } catch (SQLException e) {
-                throw new IOException("Failed to close PostgreSQL connection", e);
+                throw new IOException("Failed to close Mogdb connection", e);
             }
         }
     }
 
-    public static class PostgreSQLQueryResult implements QueryResult {
+    public static class MogdbQueryResult implements QueryResult {
         private final ResultSet resultSet;
         private final int updateCount;
 
-        PostgreSQLQueryResult(ResultSet resultSet, int updateCount) {
+        MogdbQueryResult(ResultSet resultSet, int updateCount) {
             this.resultSet = resultSet;
             this.updateCount = updateCount;
         }
@@ -165,17 +333,39 @@ public class MogDBTester implements DatabaseTester {
     }
 
     public static void main(String[] args) throws IOException {
+//        // 使用示例
+//        DBConfig dbConfig = new DBConfig.Builder()
+//            .host("localhost")
+//            .port(5432)  // Mogdb 默认端口
+//            .database("root")
+//            .user("test")
+//            .password("password")
+//            .build();
+//        MogdbTester tester = new MogdbTester(dbConfig);
+//        DatabaseConnection connection = tester.connect();
+//        QueryResult result = tester.execute(connection, "SELECT * FROM pg_user");
+//        connection.close();
+
+
         // 使用示例
         DBConfig dbConfig = new DBConfig.Builder()
-            .host("localhost")
-            .port(5432)  // PostgreSQL 默认端口
-            .database("postgres")
-            .user("postgres")
-            .password("password")
-            .build();
-        PostgreSQLTester tester = new PostgreSQLTester(dbConfig);
+                .host("localhost")
+                .port(26000)
+                .user("kbadmin")
+                .password("test")
+                .dbType("mogdb")
+                .duration(10)
+                .interval(1)
+//            .query("INSERT INTO test_table (value) VALUES ('1');")
+                .testType("executionloop")
+            .database("test_db")
+            .table("test_table")
+                .build();
+        MogDBTester tester = new MogDBTester(dbConfig);
         DatabaseConnection connection = tester.connect();
-        QueryResult result = tester.execute(connection, "SELECT * FROM pg_user");
+        String result = tester.executionLoop(connection, dbConfig.getQuery(), dbConfig.getDuration(),
+                dbConfig.getInterval(), dbConfig.getDatabase(), dbConfig.getTable());
+        System.out.println(result);
         connection.close();
     }
 }
