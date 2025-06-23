ce7132bc2240b93fecccaec169af9eb0a4cb572c
huangzhangshu <huangzhangshu@apecloud.com>
2025-06-23 17:37:34 +0800

    feat: support zookeeper executionloop test

--- src/main/java/com/apecloud/dbtester/tester/ZookeeperTester.java
+++ src/main/java/com/apecloud/dbtester/tester/ZookeeperTester.java
@@ -65,10 +65,14 @@ public class ZookeeperTester implements DatabaseTester {
             String path = parts[1];
 
             switch (operation) {
+                case "EXISTS":
+                    Stat existsStat = zk.exists(path, false);
+                    return new ZookeeperQueryResult("EXISTS", true, path, null, existsStat);
+
                 case "GET":
-                    Stat stat = new Stat();
-                    byte[] data = zk.getData(path, false, stat);
-                    return new ZookeeperQueryResult("GET", true, path, data, stat);
+                    Stat getStat = new Stat();
+                    byte[] data = zk.getData(path, false, getStat);
+                    return new ZookeeperQueryResult("GET", true, path, data, getStat);
                     
                 case "PUT":
                     if (parts.length < 3) {
@@ -140,13 +144,17 @@ public class ZookeeperTester implements DatabaseTester {
     public String connectionStress(int connections, int duration) {
         StringBuilder result = new StringBuilder();
         long startTime = System.currentTimeMillis();
-
+        String path = "/connection_stress_path";
         for (int i = 0; i < connections; i++) {
             try {
                 DatabaseConnection connection = connect();
+                if (i == 0) {
+                    execute(connection, "PUT " + path + " \"connection_stress_initial_value\"");
+                }
+
                 this.connections.add(connection);
                 // 执行一个简单的GET操作来验证连接
-                execute(connection, "GET /test_key");
+                execute(connection, "GET /connection_stress_path");
             } catch (IOException e) {
                 result.append("Failed to establish connection ").append(i).append(": ").append(e.getMessage()).append("\n");
             }
@@ -240,8 +248,141 @@ public class ZookeeperTester implements DatabaseTester {
 
     @Override
     public String executionLoop(DatabaseConnection connection, String query, int duration, int interval, String database, String table) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        ZookeeperQueryResult executeResult;
+        int executeUpdateCount;
+
+        int successfulExecutions = 0;
+        int failedExecutions = 0;
+        int disconnectCounts = 0;
+        boolean executionError = false;
+
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + duration * 1000L;
+        long errorTime = 0;
+        long recoveryTime;
+        long errorToRecoveryTime;
+        java.util.Date errorDate = null;
+        long lastOutputTime = System.currentTimeMillis();
+        int outputPassTime = 0;
+
+        int insertIndex = 0;
+        int genTestQuery = 0;
+
+        // check gen test query
+        if (query == null || query.equals("") || (table != null && !table.equals(""))) {
+            genTestQuery = 1;
+        }
+
+        if (table == null || table.equals("")) {
+            table = "executions_loop_path";  // 默认路径
+        }
+
+        String path = "/" + table;
+        System.out.println("Execution loop start: " + query);
+        while (System.currentTimeMillis() < endTime) {
+            insertIndex++;
+            long currentTime = System.currentTimeMillis();
+
+            // 定期输出统计信息
+            if (currentTime - lastOutputTime >= interval * 1000L) {
+                outputPassTime += interval;
+                lastOutputTime = currentTime;
+                System.out.println("[ " + outputPassTime + "s ] executions total: " + (successfulExecutions + failedExecutions)
+                        + " successful: " + successfulExecutions + " failed: " + failedExecutions
+                        + " disconnect: " + disconnectCounts);
+            }
+
+            try {
+                if (executionError) {
+                    Thread.sleep(1000);  // 等待一秒后尝试重连
+                    connection = connect();  // 重新建立连接
+                }
+
+                if (genTestQuery == 1) {
+                    // 检查节点是否存在，若不存在则创建
+                    executeResult = (ZookeeperQueryResult) execute(connection, "EXISTS " + path);
+                    if (executeResult.getStat() == null) {
+                        System.out.println("Path " + path + " does not exist. Creating path...");
+                        execute(connection, "PUT " + path + " \"executions_loop_initial_value\"");
+                        System.out.println("Path " + path + " created successfully.");
+                    } else {
+                        System.out.println("Path " + path + " already exists.");
+                        if (table.equals("executions_loop_path")) {
+                            // Delete path
+                            System.out.println("Delete path " + path);
+                            execute(connection, "DELETE " + path);
+                            System.out.println("Path " + path + " deleted successfully.");
+
+                            System.out.println("Create path " + path);
+                            execute(connection, "PUT " + path + " \"executions_loop_initial_value\"");
+                            System.out.println("Path " + path + " created successfully.");
+                        }
+                    }
+                    genTestQuery = 2;
+                }
+
+                if ((genTestQuery == 2 && (query == null || query.isEmpty())) || genTestQuery == 3) {
+                    String value = "executions_loop_value_" + insertIndex;
+                    query = "PUT " + path + " \"" + value + "\"";
+                    if (genTestQuery == 2) {
+                        System.out.println("Execution loop start: " + query);
+                    }
+                    genTestQuery = 3;
+                }
+
+                executeResult = (ZookeeperQueryResult) execute(connection, query);
+                executeUpdateCount = executeResult.getUpdateCount();
+                if (executeUpdateCount != -1 && executeUpdateCount != 0) {
+                    successfulExecutions++;
+                    if (executionError) {
+                        recoveryTime = System.currentTimeMillis();
+                        java.util.Date recoveryDate = new java.util.Date(recoveryTime);
+                        System.out.println("[" + sdf.format(errorDate) + "] Connection error occurred!");
+                        System.out.println("[" + sdf.format(recoveryDate) + "] Connection successfully recovered!");
+                        errorToRecoveryTime = recoveryTime - errorTime;
+                        System.out.println("The connection was restored in " + errorToRecoveryTime + " milliseconds.");
+                        executionError = false;
+                    }
+                } else {
+                    failedExecutions++;
+                    insertIndex--;
+                    executionError = true;
+                }
+            } catch (IOException e) {
+                failedExecutions++;
+                insertIndex--;
+                if (!executionError) {
+                    disconnectCounts++;
+                    errorTime = System.currentTimeMillis();
+                    errorDate = new java.util.Date(errorTime);
+                    System.out.println("[" + sdf.format(errorDate) + "] Connection error occurred!");
+                    executionError = true;
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();  // 重新设置中断状态
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("[ " + duration + "s ] executions total: " + (successfulExecutions + failedExecutions)
+                + " successful: " + successfulExecutions + " failed: " + failedExecutions
+                + " disconnect: " + disconnectCounts);
+
+        releaseConnections();  // 释放所有连接资源
+
+        result.append("Execution loop completed during ").append(duration).append(" seconds");
+
+        return String.format("Total Executions: %d\n" +
+                        "Successful Executions: %d\n" +
+                        "Failed Executions: %d\n" +
+                        "Disconnection Counts: %d",
+                successfulExecutions + failedExecutions,
+                successfulExecutions,
+                failedExecutions,
+                disconnectCounts);
     }
+
 
     private String formatQueryResult(QueryResult result) {
         if (!(result instanceof ZookeeperQueryResult)) {
@@ -263,6 +404,19 @@ public class ZookeeperTester implements DatabaseTester {
                 sb.append(String.format("Value: %s\n", zkResult.getValue()));
                 if (zkResult.getStat() != null) {
                     sb.append(String.format("Version: %d\n", zkResult.getStat().getVersion()));
+                }
+                break;
+
+            case "EXISTS":
+                sb.append("EXISTS Operation:\n");
+                if (zkResult.getValue() == null) {
+                    sb.append("No data found\n");
+                } else {
+                    sb.append(String.format("Path: %s\n", zkResult.getPath()));
+                    sb.append(String.format("Value: %s\n", zkResult.getValue()));
+                    if (zkResult.getStat() != null) {
+                        sb.append(String.format("Version: %d\n", zkResult.getStat().getVersion()));
+                    }
                 }
                 break;
     
@@ -386,4 +540,57 @@ public class ZookeeperTester implements DatabaseTester {
             return sb.toString();
         }
     }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DBConfig dbConfig = new DBConfig.Builder()
+                .host("localhost")
+                .port(2181)
+                .user("zookeeper")
+                .password("zookeeper")
+                .dbType("zookeeper")
+                .duration(10)
+                .interval(1)
+                .testType("executionloop")
+                .build();
+        ZookeeperTester tester = new ZookeeperTester(dbConfig);
+        DatabaseConnection connection = tester.connect();
+        String result = tester.executionLoop(connection, dbConfig.getQuery(),dbConfig.getDuration(),
+                dbConfig.getInterval(), dbConfig.getDatabase(), dbConfig.getTable());
+        System.out.println(result);
+        connection.close();
+
+//        dbConfig = new DBConfig.Builder()
+//                .host("127.0.0.1")
+//                .testType("query")
+//                .query("search")
+//                .dbType("zookeeper")
+//                .port(2181)
+//                .user("zookeeper")
+//                .password("zookeeper")
+//                .build();
+//
+//        String path = "/executions_loop_path";
+//
+//        System.out.println(tester.execute(connection, "PUT " + path + " \"initial_value\""));
+//
+//        ZookeeperQueryResult executeResult = (ZookeeperQueryResult) tester.execute(connection, "GET " + path);
+//
+//        if (executeResult.isSuccessful()) {
+//            System.out.println(tester.execute(connection, "Delete " + path));
+//        }
+
+//        dbConfig = new DBConfig.Builder()
+//                .host("127.0.0.1")
+//                .port(2181)
+//                .user("zookeeper")
+//                .password("zookeeper")
+//                .testType("connectionstress")
+//                .duration(10)
+//                .org("zookeeper")
+//                .build();
+//
+//        String result = tester.connectionStress(dbConfig.getConnectionCount(), dbConfig.getDuration());
+//        System.out.println(result);
+//        connection.close();
+//    }
 }
